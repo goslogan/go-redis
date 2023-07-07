d0bda26af6fb52ce0149994c2f3dbb54bcc84810
Nic Gibson <nic.gibson@redis.com>
2023-07-07 14:37:29 +0100

    docker testing - sentinel test compose complete

--- docker-testing/sentinel/create_sentinels.sh
+++ docker-testing/sentinel/create_sentinels.sh
@@ -4,20 +4,28 @@ function start_sentinel () {
   local REDIS_PORT=$1
   local PORT=$2
 
+
   mkdir -p /nodes/$PORT
 
   cat << EOF >> /nodes/${PORT}/sentinel.conf
 port ${PORT}
 daemonize yes
-sentinel monitor redis-py-test 127.0.0.1 ${REDIS_PORT} 2
-sentinel down-after-milliseconds redis-py-test 5000
-sentinel failover-timeout redis-py-test 60000
-sentinel parallel-syncs redis-py-test 1
+sentinel monitor go-redis-test 127.0.0.1 ${REDIS_PORT} 2
+sentinel down-after-milliseconds go-redis-test 5000
+sentinel failover-timeout go-redis-test 60000
+sentinel parallel-syncs go-redis-test 1
 logfile /redis.log
+pidfile /nodes/${PORT}/redis.pid
+dir /nodes/${PORT}
+protected-mode no
 EOF
 
   set -x
-  redis-sentinel /nodes/${PORT}/sentinel.conf
+  if [ ! -z "${ENABLE_DEBUG}" ]; then
+    redis-sentinel /nodes/${PORT}/sentinel.conf --enable-debug-command yes
+  else 
+    redis-sentinel /nodes/${PORT}/sentinel.conf
+  fi
   sleep 1
   if [ $? -ne 0 ]; then
     echo "Sentinel failed to start, exiting."
@@ -38,6 +46,8 @@ port ${PORT}
 daemonize yes
 logfile /redis.log
 dir /nodes/${PORT}
+pidfile /nodes/${PORT}/redis.pid
+protected-mode no
 loadmodule /opt/redis-stack/lib/redisbloom.so
 loadmodule /opt/redis-stack/lib/redisearch.so
 loadmodule /opt/redis-stack/lib/redistimeseries.so
@@ -48,11 +58,15 @@ EOF
     cat << EOFR >> /nodes/${PORT}/redis.conf
 replicaof 127.0.0.1 ${REPLICA}
 EOFR
-  fi
-  
+  fi  
+      
   cat /nodes/${PORT}/redis.conf
   set -x
-  redis-server /nodes/${PORT}/redis.conf
+  if [ ! -z "${ENABLE_DEBUG}" ]; then
+    redis-server /nodes/${PORT}/redis.conf --enable-debug-command yes
+  else 
+      redis-server /nodes/${PORT}/redis.conf
+  fi
   sleep 1
   if [ $? -ne 0 ]; then
     echo "Redis failed to start, exiting."
@@ -65,6 +79,7 @@ mkdir -p /nodes
 touch /nodes/nodemap
 
 START_PORT=$1
+ENABLE_DEBUG=$2
 
 if [ -z ${START_PORT} ]; then
     START_PORT=26379
@@ -84,7 +99,7 @@ start_sentinel ${START_PORT} $((${START_PORT}+11))
 echo "SECOND SENTINEL"
 start_sentinel ${START_PORT} $((${START_PORT}+12))
 
-echo "SECOND SENTINEL"
+echo "THIRD SENTINEL"
 start_sentinel ${START_PORT} $((${START_PORT}+13))
 
  
